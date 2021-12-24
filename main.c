826995c1cea5abef0a549fb13cc85ddffa26c95b
Az-Haris <73691596+Az-Haris@users.noreply.github.com>
2021-12-24 15:41:21 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    int Length=36, Width=18, Area;
+    Area=Length*Width;
+    printf("Rectangle Area is = %d Square Unit");
+    getch();
+    return 0;
+}
